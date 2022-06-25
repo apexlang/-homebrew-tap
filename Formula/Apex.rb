132f79e80cefb7bcee81eee876164a6b8cf7ef6c
goreleaserbot <goreleaser@carlosbecker.com>
2022-06-25 10:56:19 -0400

    Brew formula update for apex version v0.1.5

--- Formula/Apex.rb
+++ Formula/Apex.rb
@@ -5,21 +5,21 @@
 class Apex < Formula
   desc "A top-down / API-first description language for modeling and generating cloud-native applications"
   homepage "https://github.com/apexlang/cli"
-  version "0.1.4"
+  version "0.1.5"
   license "Apache-2.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/apexlang/cli/releases/download/v0.1.4/apex_darwin_arm64.tar.gz"
-      sha256 "571778e72a445913e740391ce40cfb7d8ff5ebf041c8a3e7fc71e4aa10fe29a5"
+      url "https://github.com/apexlang/cli/releases/download/v0.1.5/apex_darwin_arm64.tar.gz"
+      sha256 "bed3f6e4066c006d4e608701c14e4ac506c1c1a488152f557e1e2f549b38e472"
 
       def install
         bin.install "apex"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/apexlang/cli/releases/download/v0.1.4/apex_darwin_amd64.tar.gz"
-      sha256 "eea8fd759f1b630a40e5f717a59a02e0d6163ef0f9eab3d65ee26159ccdfe50f"
+      url "https://github.com/apexlang/cli/releases/download/v0.1.5/apex_darwin_amd64.tar.gz"
+      sha256 "cc076f50e8bcda70a133340d27084b24981c56a3ec5bf71a169376b5f1d5068e"
 
       def install
         bin.install "apex"
@@ -29,8 +29,8 @@ class Apex < Formula
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/apexlang/cli/releases/download/v0.1.4/apex_linux_amd64.tar.gz"
-      sha256 "587377df92d6a6f2b1cd7556277701d75cc05d21e3453c574ddac2d91102f437"
+      url "https://github.com/apexlang/cli/releases/download/v0.1.5/apex_linux_amd64.tar.gz"
+      sha256 "c4b2b2aad226492d5171fcc337067b163abcd0a253e2d2a76a039ec99204cddb"
 
       def install
         bin.install "apex"
